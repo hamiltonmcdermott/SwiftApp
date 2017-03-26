e96296ae83fcd2591b7efb3d31058a2377f79070
Hamilton McDermott <hamiltonmcdermott@gmail.com>
2017-03-26 13:38:38 -0700

    Changed

--- SwiftDemo1/ViewController.swift
+++ SwiftDemo1/ViewController.swift
@@ -3,21 +3,48 @@
 //  SwiftDemo1
 //
 //  Created by Hamilton McDermott on 3/21/17.
-//  Copyright © 2017 Hamilton McDermott. All rights reserved.
+//  Copyright © 2017 Hamilton McDermott. All rights reserved....
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coolLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTap(_ sender: Any) {
+        
+      tapCount = tapCount + 1
+        
+        if tapCount >= 10 {
+            coolLabel.text = "Tapped Greater than 10"
+        }
+        print(tapCount)
+        
+        
+    }
+    
+    @IBAction func coolButton(_ sender: Any) {
+        
+        
+        
+        coolLabel.text = "Buttons are Cool!"
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        
+        
     }
 
 
